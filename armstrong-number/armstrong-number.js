bfb4309ecdac168015bcbee0884b7decbf04f25a
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-28 17:25:24 +0530

    Update armstrong-number.js

--- armstrong-number/armstrong-number.js
+++ armstrong-number/armstrong-number.js
@@ -1,13 +1,13 @@
-function isArmstrong(num){
-    
-    var temp = num, sum = 0, digit;
-    
+function isArmstrong(num) {
+    var temp = num,
+        sum = 0,
+        digit;
+
     while (temp !== 0) {
-    	digit = parseInt(temp % 10);
-		sum = sum + (digit * digit * digit);
-		temp = parseInt(temp/10);
-	}
-  
-    
-    return (num === sum); // True if Success
+        digit = parseInt(temp % 10);
+        sum = sum + (digit * digit * digit);
+        temp = parseInt(temp / 10);
+    }
+
+    return (num === sum);
 }
