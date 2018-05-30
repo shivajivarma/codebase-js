877ddd28972897b5df8afb4c11600be98d33cf35
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-30 17:32:11 +0530

    Update binary-search.test.js

--- algorithms/search/binary-search/binary-search.test.js
+++ algorithms/search/binary-search/binary-search.test.js
@@ -3,7 +3,7 @@ const binarySearch = require('./binary-search');
 
 describe('binary search', () => {
   it('should find the position of number', () => {
-    assert.equals(binarySearch([1, 3, 5, 12, 44], 12), 3);
-    assert.equals(binarySearch([-1, 0, 1, 3, 5, 12, 44], 44), 6);
+    assert.equal(binarySearch([1, 3, 5, 12, 44], 12), 3);
+    assert.equal(binarySearch([-1, 0, 1, 3, 5, 12, 44], 44), 6);
   });
 });
