fa6666bc057209a578cfcc0fb4f75379df204190
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-31 18:32:28 +0530

    Create singly-linked-list.js

---
+++ data-structures/linked-lists/singly-linked-list.js
@@ -0,0 +1,55 @@
+const Node = require('./node.js');
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    /*  Function to check if list is empty  */
+    isEmpty() {
+        return this.head == null;
+    }
+
+    /*  Function to get size of list  */
+    getSize() {
+        return this.size;
+    }
+
+    /*  Function to insert an element at beginning  */
+    insertAtStart(val) {
+        let node = new Node(val, null);
+        this.size++;
+        if (this.head == null) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            node.setLink(this.head);
+            this.head = node;
+        }
+    }
+
+    /*  Function to display elements  */
+    display() {
+        var output = "Singly Linked List = ";
+        if (this.size === 0) {
+            output += "empty\n";
+        } else if (this.head.getLink() == null) {
+            output += this.head.getData();
+        } else {
+            let ptr = this.head;
+            output += this.head.getData() + "->";
+            ptr = this.head.getLink();
+            while (ptr.getLink() != null) {
+                output += ptr.getData() + "->";
+                ptr = ptr.getLink();
+            }
+            output += ptr.getData();
+        }
+        console.log(output);
+    }
+    
+}
+
+module.exports = SinglyLinkedList;
