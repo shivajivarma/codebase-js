0d3d7c5464cb6ea0f8d74ed27a605644455559ac
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-31 18:43:31 +0530

    Update linear-search.test.js

--- algorithms/search/linear-search/linear-search.test.js
+++ algorithms/search/linear-search/linear-search.test.js
@@ -6,4 +6,8 @@ describe('linear search', () => {
     assert.equal(linearSearch([1, 3, 5, 44, 12], 12), 4);
     assert.equal(linearSearch([0, 1, 44, 3, 5, 12, -1], 44), 2);
   });
+  
+  it('should not find the number', () => {
+    assert.equal(linearSearch([0, 1, 44, 3, 5, 12, -1], 88), -1);
+  });
 });
