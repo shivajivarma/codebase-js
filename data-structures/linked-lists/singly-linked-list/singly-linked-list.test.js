87db6d9b2474932321369cee0586d241d1abfae0
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2018-05-31 18:55:49 +0530

    Update singly-linked-list.test.js

--- data-structures/linked-lists/singly-linked-list/singly-linked-list.test.js
+++ data-structures/linked-lists/singly-linked-list/singly-linked-list.test.js
@@ -10,9 +10,12 @@ describe('singly linked list', () => {
 
   it('insert at the start of linked list', () => {
     let list = new SinglyLinkedList();
-    list.insertAtStart(12);
-    assert.equal(list.getSize(), 1);
+    list.insertAtStart(99);
+    list.insertAtStart(13);
+    let arr = list.toArray();
+    assert.equal(list.getSize(), 2);
+    assert.equal(arr[0], 13);
+    assert.equal(arr[1], 99);
   });
 
-  
 });
